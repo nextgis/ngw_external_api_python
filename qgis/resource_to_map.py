409b6c9cbdeedf6cdd8e77b0bc1f80e0b4feae53
yellow-sky <nikulin.e@gmail.com>
2015-02-03 19:16:51 +0300

    WFS Layers append to the map method added

--- qgis/resource_to_map.py
+++ qgis/resource_to_map.py
@@ -18,9 +18,10 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.core import QgsVectorLayer, QgsMapLayerRegistry, QgsMapLayer
+from qgis.core import QgsVectorLayer, QgsMapLayerRegistry, QgsMapLayer, QgsProject, QgsRectangle
 from ..core.ngw_error import NGWError
 from ..core.ngw_vector_layer import NGWVectorLayer
+from ..core.ngw_wfs_service import NGWWfsService
 
 
 def add_resource_as_geojson(resource, return_extent=False):
@@ -38,3 +39,38 @@ def add_resource_as_geojson(resource, return_extent=False):
         if qgs_geojson_layer.extent().isEmpty() and qgs_geojson_layer.type() == QgsMapLayer.VectorLayer:
             qgs_geojson_layer.updateExtents()
             return qgs_geojson_layer.extent()
+
+
+def add_resource_as_wfs_layers(wfs_resource, return_extent=False):
+    if not isinstance(wfs_resource, NGWWfsService):
+        raise NGWError('Resource type is not WfsService!')
+    #Extent stuff
+    if return_extent:
+        summary_extent = QgsRectangle()
+        summary_extent.setMinimal()
+    #Add group
+    toc_root = QgsProject.instance().layerTreeRoot()
+    layers_group = toc_root.insertGroup(0, wfs_resource.common.display_name)
+    #Add layers
+    for wfs_layer in wfs_resource.wfs.layers:
+        url = wfs_resource.get_wfs_url(wfs_layer.keyname) + '&srsname=EPSG:3857&VERSION=1.0.0&REQUEST=GetFeature'
+        qgs_wfs_layer = QgsVectorLayer(url, wfs_layer.display_name, 'WFS')
+        #summarize extent
+        if return_extent:
+            _summ_extent(summary_extent, qgs_wfs_layer)
+        QgsMapLayerRegistry.instance().addMapLayer(qgs_wfs_layer, False)
+        layers_group.insertLayer(0, qgs_wfs_layer)
+
+    if return_extent:
+        return summary_extent
+
+
+def _summ_extent(self, summary_extent, layer):
+    layer_extent = layer.extent()
+
+    if layer_extent.isEmpty() and layer.type() == QgsMapLayer.VectorLayer:
+        layer.updateExtents()
+        layer_extent = layer.extent()
+
+    if layer_extent.isNull():
+        return
