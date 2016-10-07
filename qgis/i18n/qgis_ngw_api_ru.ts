13b34340c7340956f60aa19ddfe674e3a0ea4fde
alisovenko <alexander.lisovenko@gmail.com>
2016-10-07 14:49:54 +0300

    Add translation file

---
+++ qgis/i18n/qgis_ngw_api_ru.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>CurrentQGISProjectImporter</name>
+    <message>
+        <location filename="ngw_resource_model_4qgis.py" line="501"/>
+        <source>Import &apos;%s&apos;</source>
+        <translation>Импорт &apos;%s&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="32"/>
+        <source>Create new connection</source>
+        <translation>Создать новое подключение</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="41"/>
+        <source>Connection details</source>
+        <translation>Параметры подключения</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="66"/>
+        <source>connection addres (e.g. mygis.nextgis.com)</source>
+        <translation>адрес подключения (например, mygis.nextgis.com)</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="75"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="82"/>
+        <source>As guest</source>
+        <translation>Как гость</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="114"/>
+        <source>Connection testing</source>
+        <translation>Проверка подключения</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="127"/>
+        <source>Name</source>
+        <translation>Имя</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="147"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="161"/>
+        <source>administrator</source>
+        <translation>administrator</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog_base.ui" line="168"/>
+        <source>User name</source>
+        <translation>Имя пользователя</translation>
+    </message>
+</context>
+<context>
+    <name>NGWConnectionEditDialog</name>
+    <message>
+        <location filename="ngw_connection_edit_dialog.py" line="198"/>
+        <source>Fill it!</source>
+        <translation>Заполните поле!</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog.py" line="239"/>
+        <source>Connection test...</source>
+        <translation>Проверка подключения...</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog.py" line="245"/>
+        <source>Connection success.</source>
+        <translation>Успешное подключение.</translation>
+    </message>
+    <message>
+        <location filename="ngw_connection_edit_dialog.py" line="249"/>
+        <source>Specified URL webgis not found! Or your webgis version is below 3</source>
+        <translation>По заданному URL Веб ГИС не найдена! Или версия Вашей Веб ГИС ниже 3</translation>
+    </message>
+</context>
+<context>
+    <name>QGISResourceJob</name>
+    <message>
+        <location filename="ngw_resource_model_4qgis.py" line="276"/>
+        <source>We&apos;ve renamed fields {0} for layer &apos;{1}&apos;. Style for this layer may become invalid.</source>
+        <translation>Следующие поля переименованы {0} for layer &apos;{1}&apos;. Стиль для этого слоя может стать некорректным.</translation>
+    </message>
+</context>
+</TS>
