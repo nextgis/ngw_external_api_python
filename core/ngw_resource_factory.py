9fd06b9a81a2018d3451fbb6f72a85e00d343e21
alisovenko <alexander.lisovenko@gmail.com>
2017-10-19 10:30:44 +0300

    Fix bugs in upload file process Fix requests dependency

--- core/ngw_resource_factory.py
+++ core/ngw_resource_factory.py
@@ -30,7 +30,6 @@ from ngw_raster_style import NGWRasterStyle
 from ngw_group_resource import NGWGroupResource
 from ngw_wfs_service import NGWWfsService
 from ngw_resource import NGWResource
-from ngw_connection import NGWConnection
 from ngw_base_map import NGWBaseMap
 
 API_NGW_VERSION = '/api/component/pyramid/pkg_version'
--- qgis/qgis_ngw_connection.py
+++ qgis/qgis_ngw_connection.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+    NextGIS WEB API
+                              -------------------
+        begin                : 2014-11-19
+        git sha              : $Format:%H$
+        copyright            : (C) 2014 by NextGIS
+        email                : info@nextgis.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import json
 from base64 import b64encode
 
@@ -62,7 +82,7 @@ class QgsNgwConnection(QObject):
         file = kwargs.get("file")
 
         log(
-            "Request\nmethod: {}\nurl: {}\njson({}): {}\nfile: {}".format(
+            u"Request\nmethod: {}\nurl: {}\njson({}): {}\nfile: {}".format(
                 method,
                 self.server_url + sub_url,
                 type(json_data),
