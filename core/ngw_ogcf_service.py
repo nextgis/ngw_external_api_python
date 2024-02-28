f1ee349f2691863b4cc9514abea54f12c90d8204
Ivan Barsukov <i.barsukov@protonmail.com>
2024-02-29 01:12:01 +0700

    Fix creds for OGCF

--- core/ngw_ogcf_service.py
+++ core/ngw_ogcf_service.py
@@ -1,8 +1,8 @@
-from typing import Tuple, Optional
-
 from qgis.core import QgsDataSourceUri
 
 from .ngw_resource import NGWResource, DICT_TO_OBJ, LIST_DICT_TO_LIST_OBJ
+
+from nextgis_connect.ngw_connection.ngw_connections_manager import NgwConnectionsManager
 
 
 class NGWOgcfService(NGWResource):
@@ -20,28 +20,20 @@ class NGWOgcfService(NGWResource):
             lambda layer: layer.keyname == layer_keyname, self.ogcf.layers
         ))
 
+        connections_manager = NgwConnectionsManager()
+        connection = connections_manager.connection(self.connection_id)
+
         uri = QgsDataSourceUri()
-        creds = self.get_creds_for_url()
-        if creds[0] and creds[1]:
-            uri.setUsername(creds[0])
-            uri.setPassword(creds[1])
         uri.setParam('typename', layer_keyname)
         uri.setParam('srsname', 'OGC:CRS84')
         uri.setParam('preferCoordinatesForWfsT11', 'false')
         uri.setParam('pagingEnabled', 'false')
         uri.setParam('maxNumFeatures', str(layer.maxfeatures))
         uri.setParam('restrictToRequestBBOX', '1')
+        uri.setParam('authcfg', connection.auth_config_id)
         uri.setParam('url', self.get_absolute_api_url() + '/ogcf')
 
         return uri.uri(True)
-
-    def get_creds_for_url(self) -> Tuple[Optional[str], Optional[str]]:
-        creds = self.get_creds()
-        if not creds[0] or not creds[1]:
-            return creds
-        login = creds[0]
-        password = creds[1].replace('&', '%26')
-        return login, password
 
     def get_layers(self):
         return self.ogcf.layers
