db4d946aac86e52d12275796f51af22be4d761d1
Ivan Barsukov <i.barsukov@protonmail.com>
2024-09-26 17:45:45 +0700

    Add qml downloading

--- core/ngw_qgis_style.py
+++ core/ngw_qgis_style.py
@@ -18,10 +18,57 @@
  ***************************************************************************/
 """
 
+from typing import Optional
+
+from qgis.core import QgsNetworkAccessManager
+from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt.QtNetwork import QNetworkRequest
+
+from nextgis_connect.exceptions import NgwError
+from nextgis_connect.logging import logger
+from nextgis_connect.ngw_connection.ngw_connections_manager import (
+    NgwConnectionsManager,
+)
+
 from .ngw_resource import NGWResource
 
 
 class NGWQGISStyle(NGWResource):
+    def __init__(self, resource_factory, resource_json):
+        super().__init__(resource_factory, resource_json)
+        self.__qml = None
+
+    @property
+    def is_qml_populated(self) -> bool:
+        return self.__qml is not None
+
+    def populate_qml(self) -> None:
+        if self.__qml is not None:
+            return
+
+        logger.debug(f"Download qml style with id={self.resource_id}")
+
+        qml_url = self.download_qml_url()
+        qml_req = QNetworkRequest(QUrl(qml_url))
+
+        connections_manager = NgwConnectionsManager()
+        connection = connections_manager.connection(self.connection_id)
+        connection.update_network_request(qml_req)
+
+        dwn_qml_manager = QgsNetworkAccessManager()
+        reply_content = dwn_qml_manager.blockingGet(qml_req, forceRefresh=True)
+
+        if reply_content.error():
+            raise NgwError(
+                f"Failed to download QML: {reply_content.errorString()}"
+            )
+
+        self.__qml = reply_content.content().data().decode()
+
+    @property
+    def qml(self) -> Optional[str]:
+        return self.__qml
+
     def download_qml_url(self):
         return self.get_absolute_api_url() + "/qml"
 
