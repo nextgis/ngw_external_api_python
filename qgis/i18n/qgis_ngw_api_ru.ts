912ec330b1791721fbab3e4d9293eb425bad012e
Ivan Barsukov <i.barsukov@protonmail.com>
2025-03-31 10:13:31 +0300

    Update translations

--- qgis/i18n/qgis_ngw_api_ru.ts
+++ qgis/i18n/qgis_ngw_api_ru.ts
@@ -4,87 +4,87 @@
 <context>
     <name>QGISResourceJob</name>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="302"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="303"/>
         <source>create WMS connection</source>
         <translation>создание соединения WMS</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="348"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="349"/>
         <source>creating WMS layer</source>
         <translation>создание слоя WMS</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1571"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1606"/>
         <source>creating</source>
         <translation>создание</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1829"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1864"/>
         <source>adding aliases</source>
         <translation>добавление псевдонимов</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1839"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1874"/>
         <source>finishing</source>
         <translation>завершение</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="566"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="576"/>
         <source>preparing</source>
         <translation>подготовка</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="916"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="951"/>
         <source>Style for &quot;{}&quot;</source>
         <translation>Стиль для &quot;{}&quot;</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1059"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1094"/>
         <source>removing all features</source>
         <translation>удаление всех объектов</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1759"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1794"/>
         <source>uploading ({}%)</source>
         <translation>загрузка ({}%)</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="603"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="638"/>
         <source>checking geometry ({}%)</source>
         <translation>проверка геометрии ({}%)</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="727"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="762"/>
         <source>preparing layer ({}%)</source>
         <translation>подготовка слоя ({}%)</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="839"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="874"/>
         <source>Feature {} haven&apos;t been added. Please check geometry</source>
         <translation>Объект {} не был добавлен. Пожалуйста, проверьте геометрию</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1078"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1113"/>
         <source>adding features ({}%)</source>
         <translation>добавление объектов ({}%)</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="514"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="515"/>
         <source>adding lookup tables</source>
         <translation>добавление словарей</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1289"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1324"/>
         <source>A group tree is being created</source>
         <translation>Создаётся дерево ресурсов</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1585"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1620"/>
         <source>Failed to load any resource to the NextGIS Web. Webmap will not be created</source>
         <translation>При загрузке ресурсов в NextGIS Web произошла ошибка. Веб-карта не будет создана</translation>
     </message>
     <message>
-        <location filename="../ngw_resource_model_4qgis.py" line="1809"/>
+        <location filename="../ngw_resource_model_4qgis.py" line="1844"/>
         <source>replacing features</source>
         <translation>замена объектов</translation>
     </message>
@@ -92,14 +92,14 @@
 <context>
     <name>QgsNgwConnection</name>
     <message>
-        <location filename="../qgis_ngw_connection.py" line="434"/>
+        <location filename="../qgis_ngw_connection.py" line="431"/>
         <source>File is too large for uploading</source>
         <translation>Загружаемый файл слишком большой</translation>
     </message>
     <message>
-        <location filename="../qgis_ngw_connection.py" line="321"/>
-        <source>Connection closed by QGIS. Increase timeout (Settings -&gt; Options -&gt; Network) to 300000 and retry.</source>
-        <translation>Соединение прервано QGIS. Увеличьте таймаут для сетевых запросов (Установки -&gt; Параметры -&gt; Сеть) до 300000 и повторите попытку.</translation>
+        <location filename="../qgis_ngw_connection.py" line="316"/>
+        <source>Connection was closed by QGIS. Please check your internet connection or increase timeout (Settings -&gt; Options -&gt; Network) and retry.</source>
+        <translation>Соединение было закрыто QGIS. Пожалуйста, проверьте настройки вашего интернет-соединения или увеличьте таймаут для сетевых запросов (Установки -&gt; Параметры -&gt; Сеть) и повторите попытку.</translation>
     </message>
 </context>
 </TS>
